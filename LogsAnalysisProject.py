1c95918d1c51e62d2e844716214aa3475b7777ee
Jose Ramirez <jdevla@protonmail.com>
2017-10-11 08:50:20 -0700

    Pep8 approved

--- LogsAnalysisProject.py
+++ LogsAnalysisProject.py
@@ -2,6 +2,7 @@ import psycopg2
 
 DB_NAME = "news"
 
+
 def dbconnect():
     db = psycopg2.connect("dbname=news")
     cur = db.cursor()
@@ -10,7 +11,11 @@ def dbconnect():
 
 def top_articles():
     db, cur = dbconnect()
-    cur.execute("SELECT articles.title, count(log.path) as amount FROM articles, log WHERE log.path Like concat('%', articles.slug, '%') GROUP BY articles.title ORDER BY amount desc limit 3;")
+    cur.execute("""SELECT articles.title, count(log.path) as amount
+                FROM articles, log
+                WHERE log.path Like concat('%', articles.slug, '%')
+                GROUP BY articles.title
+                ORDER BY amount desc limit 3;""")
     results = cur.fetchall()
     print "1. Three most popular articles of all time"
     print "-" * 50
@@ -21,7 +26,13 @@ def top_articles():
 
 def top_authors():
     db, cur = dbconnect()
-    cur.execute("SELECT authors.name, count(log.status) as total FROM authors, articles, log WHERE articles.author = authors.id and log.status = '200 OK' and log.path like concat('%', articles.slug, '%') GROUP BY authors.name ORDER BY total desc;")
+    cur.execute("""SELECT authors.name, count(log.status) as total
+                FROM authors, articles, log
+                WHERE articles.author = authors.id
+                AND log.status = '200 OK' and log.path
+                LIKE concat('%', articles.slug, '%')
+                GROUP BY authors.name
+                ORDER BY total desc;""")
     results = cur.fetchall()
     print "\n2. Who are the most popular article authors of all time?"
     print "-" * 50
@@ -29,10 +40,15 @@ def top_authors():
         print line
     db.close()
 
-    
+
 def top_error():
     db, cur = dbconnect()
-    cur.execute("SELECT to_char(http_cuatros.date, 'MM/DD/YYYY'), round((http_cuatros.numbers*1.0 / add_all.numbers*1.0)*100, 2) per FROM http_cuatros, add_all WHERE http_cuatros.date = add_all.date and (http_cuatros.numbers*1.0 / add_all.numbers*1.0)*100 > 1 ORDER BY per desc;")
+    cur.execute("""SELECT to_char(http_cuatros.date, 'MM/DD/YYYY'),
+                ROUND((http_cuatros.numbers/ add_all.numbers) * 100,  2) per
+                FROM http_cuatros, add_all
+                WHERE http_cuatros.date = add_all.date
+                AND (((http_cuatros.numbers / add_all.numbers) * 100) > 1)
+                ORDER BY per desc;""")
     results = cur.fetchall()
     print "\n3. On which days did more than 1% of requests lead to errors?"
     print "-" * 50
@@ -40,7 +56,7 @@ def top_error():
         print "On " + str(line[0]) + " had " + str(line[1]) + "% of errors"
     db.close()
 
-    
+
 top_articles()
 top_authors()
 top_error()
